71f3d45c4d8cd365c9c3996c7f0714de812bd431
d.novikov <d.novikov@bars.group>
2023-09-08 14:04:08 +0700

    OTUS HOMEWORK 5

--- src/router/router.js
+++ src/router/router.js
@@ -1,55 +1,64 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Catalog from '../components/Catalog.vue';
-import Product from '../components/Product.vue';
-import Cart from '../components/Cart.vue';
-import Checkout from '../components/Checkout.vue';
-import Login from '../components/Login.vue';
-import AddNewProduct from '../components/AddNewProduct.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Catalog from "../components/Catalog.vue";
+import Product from "../components/Product.vue";
+import Cart from "../components/Cart.vue";
+import Checkout from "../components/Checkout.vue";
+import Login from "../components/Login.vue";
+import AddNewProduct from "../components/AddNewProduct.vue";
+import { useStore } from "../stores/index.js";
 
 const routes = [
-    {
-        path: '/home',
-        name: 'Home',
-        component: Catalog
+  {
+    path: "/home",
+    name: "Home",
+    component: Catalog,
+  },
+  {
+    path: "/",
+    name: "Catalog",
+    component: Catalog,
+  },
+  {
+    path: "/product/:id",
+    component: Product,
+    name: "Product",
+    props: true,
+  },
+  {
+    path: "/cart",
+    component: Cart,
+    name: "Cart",
+    props: true,
+  },
+  {
+    path: "/checkout",
+    component: Checkout,
+    name: "Checkout",
+    props: true,
+  },
+  {
+    path: "/login",
+    component: Login,
+    name: "Login",
+    props: true,
+  },
+  {
+    path: "/add_new_product",
+    component: AddNewProduct,
+    name: "AddNewProduct",
+    props: true,
+    beforeEnter: (to, from, next) => {
+      const store = useStore();
+      if (store.isAuth) {
+        next();
+      } else {
+        next({ path: "/" });
+      }
     },
-    {
-        path: '/',
-        name: 'Catalog',
-        component: Catalog
-    },
-    {
-        path: '/product/:id',
-        component: Product,
-        name: 'Product',
-        props: true
-    },
-    {
-        path: '/cart',
-        component: Cart,
-        name: 'Cart',
-        props: true
-    },
-    {
-        path: '/checkout',
-        component: Checkout,
-        name: 'Checkout',
-        props: true
-    },
-    {
-        path: '/login',
-        component: Login,
-        name: 'Login',
-        props: true
-    },
-    {
-        path: '/add_new_product',
-        component: AddNewProduct,
-        name: 'AddNewProduct',
-        props: true
-    },
+  },
 ];
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
 export default router;
